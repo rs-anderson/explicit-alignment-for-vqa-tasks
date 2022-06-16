809c05e8208379a18de5d3b9a7f9422158f77514
rs-anderson <rsa39@cam.ac.uk>
2022-06-16 14:45:33 +0100

    add: script for extracting clip image embeddings

--- src/tools/extract_contrastive_image_embeddings.py
+++ src/tools/extract_contrastive_image_embeddings.py
@@ -14,58 +14,78 @@ data_dir = Path("../../data")
 vqa2_data_dir = data_dir / "vqa2"
 okvqa_data_dir = data_dir / "ok-vqa"
 
-def main(clip_model_name: str, split: str="val"):
 
-    device = torch.device('cuda:0')
-    
+def main(clip_model_name: str, subtype: str = "val2014"):
+
+    print(f"Extracting {subtype} using {clip_model_name}")
+
     # clip_model_name = clip_model_type.replace('/', '_')
-    out_path = vqa2_data_dir / "pre-extracted_features" / f"coco_{clip_model_name}_{split}.pkl"
-    
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    image_preprocessor = CLIPFeatureExtractor.from_pretrained("openai/clip-vit-base-patch32")
+    out_path = (
+        vqa2_data_dir
+        / "pre-extracted_features"
+        / f"coco_{clip_model_name}_{subtype}.pkl"
+    )
 
-    with open(vqa2_data_dir / f'v2_OpenEnded_mscoco_{split}2014_questions.json', 'r') as f:
+    model = CLIPModel.from_pretrained(f"openai/{clip_model_name}")
+    image_preprocessor = CLIPFeatureExtractor.from_pretrained(
+        f"openai/{clip_model_name}"
+    )
+
+    with open(
+        vqa2_data_dir / f"v2_OpenEnded_mscoco_{subtype}_questions.json", "r"
+    ) as f:
         data = json.load(f)
-    
-    # TODO: extract only "questions" from data dictionary
-    print("%0d questions loaded from json " % len(data))
 
-    all_embeddings = []
-    all_captions = []
-    
-    for i in tqdm(range(len(data))):
-        data_item = data[i]
-        
-        img_id = data_item["image_id"]
-        img_path = okvqa_data_dir / f"{split}2014/COCO_{split}2014_{int(img_id):012d}.jpg"
-        
-        img_key_full = str(img_id).zfill(12)
-        img = io.imread(img_path)
+    assert data["data_subtype"] == subtype, "wrong dataset subtype was loaded"
+    print(data["info"])
 
-        image = image_preprocessor(Image.fromarray(image)).unsqueeze(0).to(device)
+    questions = data["questions"]
+    print("%0d questions loaded from json " % len(questions))
+
+    img_ids_with_embeddings = {}
+
+    # got to 96755/443757
+    for i in tqdm(range(len(questions))):
+        data_item = questions[i]
+
+        img_id = str(data_item["image_id"])
+
+        if img_id in img_ids_with_embeddings:
+            continue
+
+        img_path = (
+            okvqa_data_dir / f"{subtype}/COCO_{subtype}_{int(img_id):012d}.jpg"
+        )
+        image = io.imread(img_path)
+
+        image = image_preprocessor(
+            Image.fromarray(image), return_tensors="pt"
+        )["pixel_values"]
         with torch.no_grad():
-            prefix = model.get_image_features(image).cpu()
-        
-        data_item["clip_embedding"] = i
-        
-        all_embeddings.append(prefix)
-        all_captions.append(data_item)
-        
+            prefix = model.get_image_features(image).numpy()
+
+        img_ids_with_embeddings[img_id] = prefix
+
         if (i + 1) % 10000 == 0:
-            with open(out_path, 'wb') as f:
-                pickle.dump({"clip_embedding": torch.cat(all_embeddings, dim=0), "captions": all_captions}, f)
+            with open(out_path, "wb") as f:
+                pickle.dump(img_ids_with_embeddings, f)
 
-    with open(out_path, 'wb') as f:
-        pickle.dump({"clip_embedding": torch.cat(all_embeddings, dim=0), "captions": all_captions}, f)
+    with open(out_path, "wb") as f:
+        pickle.dump(img_ids_with_embeddings, f)
 
-    print('Done')
-    print("%0d embeddings saved " % len(all_embeddings))
-    return 0
+    print("Done")
+    print("%0d embeddings saved " % len(img_ids_with_embeddings))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--clip_model_type', default="clip-vit-base-patch32", choices=('clip-vit-base-patch32'))
-    parser.add_argument('--split', default="val", choices=('train', 'val'))
+    parser.add_argument(
+        "--clip_model_type",
+        default="clip-vit-base-patch32",
+        choices=("clip-vit-base-patch32"),
+    )
+    parser.add_argument(
+        "--split", default="val2014", choices=("train2014", "val2014")
+    )
     args = parser.parse_args()
-    exit(main(args.clip_model_type))
+    main(args.clip_model_type, args.split)
